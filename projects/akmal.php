162f477491bbd010ad36875427850c88d0bb52cd
Akmal Kubaev <akmal.kubaev@gmail.com>
2020-09-17 10:23:19 -0400

    added bio about me

--- projects/akmal.php
+++ projects/akmal.php
@@ -1,4 +1,25 @@
 <?php
 #Author: Akmal Kubaev
 #Company: MYITEDU, LLC
-#Date: Sep 14, 20Aii<?php
+#Date: Sep 14, 2020
+
+hello fellas,
+My name is Akmal Kubaev and I am 25 years young.
+Let me introduce myself to you all. I am from lovely and sunny Uzbekistan.
+Over past years I am grown up with passion of learning new things and challenge myself to deal problems
+and in any way to solve them by the time. I can tell about me that I am a mysterious box that
+every time you open, you will get new me.
+Nowadays, with the recomendations of good people and friends, I started to learning computer programming
+that absolutely new chapter for my life. Day by day, this interest about programming and software engineerning
+going to top of my plans for the basis of lifestructure at least for the next 10 years.
+If I clarify more openly about me, I am from Jizakh and most of my childhood adventures belongs there.
+I graduated school and academic lyceum overthere. My bachelor degree is from Riga, Latvia. which is
+also involved with full of fun and interesting stories. And it is specialized for Technical Translation of Economics.
+In US I have been to 3 years almost and finally I guess I find my passion. And for this passion, I will need
+your help and cooperation. By teamworking we will speed up our skills and knowledge.
+Good luck everyone,
+Best regards!
+Akmal Kubaev.
+
+
+
