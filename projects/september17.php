904eec026d68402db67211e5f81092935286dd06
MYITEDU <myitedu@localhosts>
2020-09-17 11:45:16 -0400

    Jon midified the sept file by removing old lines and added new

--- projects/september17.php
+++ projects/september17.php
@@ -1,14 +1,12 @@
-Jon: 10:46 am
 Odamboy 10:49-hii all
 Akmal: 10:53 am
 Zuhriddin: 11:20
-Jon: 11:21 AM - Hello
 Odamboy 11:31AM-Merhaba
 Sayriddin: 10:20 am
 Odamboy 10:49
 Akmal: 10:53 am
 Zuhriddin: 11:20
-Jon: 11:21 AM - Hello
 Sayriddin -hello 10.30 am
 zuhriddin hello everybody
-
+Admin: Simon says go to the mailbox and bring me all the mails
+Admin: It is day time here and it is night somewhere at 11:44 AM EST
